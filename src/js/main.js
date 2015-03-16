734ea80f501487b3f26b0727cea7561d68986dfc
kenji-special <k.saito.1985@gmail.com>
2015-03-16 14:59:42 +0900

    changed components folder into views folder.

--- src/js/main.js
+++ src/js/main.js
@@ -1,6 +1,6 @@
 var React = require('react');
 
-var App = require('./components/example.jsx'); // need to specify the jsx extension
+var App = require('./views/example.jsx'); // need to specify the jsx extension
 
 React.render(<App/>, document.body);
 
